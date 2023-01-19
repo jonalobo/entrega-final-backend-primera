ac56ba2225ffdd71c6ee9713d02e575390c8cc3a
Jonathan Alvarado <jonalobo98@gmail.com>
2023-01-18 22:34:36 -0600

    optimizacion

--- controller/productosControlador.js
+++ controller/productosControlador.js
@@ -3,19 +3,19 @@ const moment = require("moment");
 
 const { Producto, productos, leer, buscarProductoId, escribir, /* actualizarPorId */ } = require('../helpers/claseProductos')
 
-
+//Metodo http
 const obtenerTodosControlador = async ( req = request , res = response)=>{
     //Acá debo traer los productos en la persistencia del FS
     let productoRespuesta = await leer()
     res.json(productoRespuesta)
 }
-
+//Metodo http
 const obtenerPorIdControlador =async (req,res)=>{
     const {id} = req.params
     let productoSeleccionado = await buscarProductoId(id)
     res.json(productoSeleccionado)
 }
-
+//Metodo http
 const agregarProductoControlador = (req = request,res = response)=>{
     const {nombre,descripcion,codigo,fotoUrl,precio,stock} = req.body
     //Envio el json a la clase producto asignandolo a una constante.
@@ -25,7 +25,7 @@ const agregarProductoControlador = (req = request,res = response)=>{
     //Devuelvo los objetos guardados en el arreglo de la clase Producto
     res.json(productos)
 }
-
+//Metodo http
 const actualizarProductosControlador = async (req = request, res = response)=>{
     const {id} = req.params
     const {nombre, descripcion, codigo, fotoUrl, precio, stock} = req.body
