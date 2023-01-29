92cb486bc4e8000dcb35e3b04d9253612f0fb951
Jonathan Alvarado <jonalobo98@gmail.com>
2023-01-28 18:05:56 -0600

    Listas las rutas del carrito todo probado en postman

--- routers/carritoRuta.js
+++ routers/carritoRuta.js
@@ -7,13 +7,13 @@ const rutaCarrito = Router()
 //Ruta para obtener productos según el carrito elegido
 rutaCarrito.get('/carrito/:id/productos',listarProductosPorIdCarrito)
 
-
+//Ruta para crear un nuevo carrito
 rutaCarrito.post('/carrito',crearCarrito)
 
-
+//Ruta para agregar un producto según su id al carrito
 rutaCarrito.post('/carrito/:id/productos',agregarProductoACarrito)
 
-
+//Ruta para eliminar un carrito
 rutaCarrito.delete('/carrito/:id',borrarCarritoPorId)
 
 module.exports = rutaCarrito
